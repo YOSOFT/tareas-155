221ce878c291fc7c65ad53ebb7110be40d849978
YOSOFT <julio.ramirez@yosoft.com.co>
2018-09-11 16:55:40 -0500

    handler

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
+import { TareaProvider } from '../../providers/tarea/tarea';
 
 @Component({
   selector: 'page-home',
@@ -9,12 +10,13 @@ export class HomePage {
   tareas = [];
   constructor
   (public navCtrl: NavController,
-    private alerta: AlertController
+    private alerta: AlertController,
+    private servicioTarea: TareaProvider
     ) {
-
+      this.tareas = servicioTarea.obtenerTareas();
   }
   agregarTarea(){
-    this.tareas.push("texto");
+    // this.tareas.push("texto");
     let alert = this.alerta.create({
       title: "Agregar tarea",
       inputs: [{
@@ -26,7 +28,8 @@ export class HomePage {
         {
           text: "Agregar",
           handler: (datos) => { console.log(datos);
-            this.tareas.push(datos.textoTarea);
+            // this.tareas.push(datos.textoTarea);
+            this.servicioTarea.agregarTareas(datos.textoTarea);
           }
         }
       ]
