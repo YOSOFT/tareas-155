ea38f614ce798409a10a64698ac69752877485f9
YOSOFT <julio.ramirez@yosoft.com.co>
2018-09-11 15:52:10 -0500

    tareas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  tareas = [];
   constructor(public navCtrl: NavController) {
 
   }
-
+  agregarTarea(){
+    this.tareas.push("texto");
+  }
 }
